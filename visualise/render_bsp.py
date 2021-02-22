c43b582f38d9e867512c23890c6dda9a2cb2a72f
snake-biscuits <36507175+snake-biscuits@users.noreply.github.com>
2021-02-22 19:48:32 +1000

    pep8 cleanup

--- visualise/render_bsp.py
+++ visualise/render_bsp.py
@@ -1,17 +1,14 @@
-﻿import colorsys
 import ctypes
 import itertools
-import math
+import os
 import time
-import struct
-import sys
 
 import bsp_tool
 import numpy as np
 from OpenGL.GL import *
 from OpenGL.GL.shaders import compileShader, compileProgram
 from OpenGL.GLU import *
-from sdl2 import *
+from sdl2 import *  # .dll import is busted
 
 from utilities import camera
 from utilities import vector
@@ -19,28 +16,34 @@ from utilities import vector
 
 camera.sensitivity = 2
 
+
 def fan_range(start, length):
     out = []
     for i in range(start + 2, start + length):
         out.extend([start, i - 1, i])
     return out
 
+
 # TODO: select shaders and triangulation mode from bsp version & mod
 def main(width, height, bsp):
     SDL_Init(SDL_INIT_VIDEO)
-    window = SDL_CreateWindow(bytes(bsp.filename, "utf-8"), SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, width, height, SDL_WINDOW_OPENGL) #| SDL_WINDOW_BORDERLESS) #SDL_WINDOW_FULLSCREEN
+    window = SDL_CreateWindow(bytes(bsp.filename, "utf-8"),
+                              SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+                              width, height, SDL_WINDOW_OPENGL)
     glContext = SDL_GL_CreateContext(window)
 
     glClearColor(.5, .5, .5, 0)
-##    glEnable(GL_CULL_FACE)
+    # glEnable(GL_CULL_FACE)
     glEnable(GL_DEPTH_TEST)
     glFrontFace(GL_CW)
     glPointSize(4)
     glPolygonMode(GL_BACK, GL_LINE)
     gluPerspective(120, width / height, 0.1, 1000000)
-    
+
     conversion_start = time.time()
-    if bsp.bsp_version == 20: # Team Fortress 2 FACES
+
+    # Team Fortress 2 FACES
+    if bsp.bsp_version == 20:
         # TODO: indentify materials & location in buffer
         # -- this can allow for transparent trigger brushes
         # -- alternatively, identify faces from the model lump
@@ -52,12 +55,12 @@ def main(width, height, bsp):
             texture_name = bsp.TEXDATA_STRING_DATA[tex_data.tex_data_string_index]
             if "TRIGGER" in texture_name:
                 continue
-##            huh = face.first_edge
-##            heh = bsp.SURFEDGES[huh : (huh + face.num_edges)]
-##            ugh = [bsp.EDGES[ass][0] if ass >= 0 else bsp.EDGES[-ass][1] for ass in heh][::2]
-##            oof = {ugh.count(wha) for wha in ugh}
-##            if oof != {1}:
-##                continue
+            # huh = face.first_edge
+            # heh = bsp.SURFEDGES[huh:(huh + face.num_edges)]
+            # ugh = [bsp.EDGES[ass][0] if ass >= 0 else bsp.EDGES[-ass][1] for ass in heh][::2]
+            # oof = {ugh.count(wha) for wha in ugh}
+            # if oof != {1}:
+            #     continue
             if face.disp_info == -1:
                 face_vertices = bsp.vertices_of_face(face_index)
                 indices.extend(fan_range(len(vertices), len(face_vertices)))
@@ -69,7 +72,9 @@ def main(width, height, bsp):
                 indices.extend([len(vertices) + i for i in disp_indices])
                 vertices.extend(face_vertices)
         vertices = list(itertools.chain(*[itertools.chain(*v) for v in vertices]))
-    elif bsp.bsp_version >= 37: # Titanfall 2 & Apex Legends MESHES
+
+    # Titanfall 2 & Apex Legends MESHES
+    elif bsp.bsp_version >= 37:
         vertices = []
         indices = []
         for mesh_index, mesh in enumerate(bsp.MESHES):
@@ -99,17 +104,16 @@ def main(width, height, bsp):
     render_mode = "flat"
     major, minor = glGetIntegerv(GL_MAJOR_VERSION), glGetIntegerv(GL_MINOR_VERSION)
     print(f"OpenGL Version {major}.{minor}")
-    if major >= 4: # Assuming GLSL 450
-        USING_ES = False
-        shader_folder += "450_CORE"    
-    elif major == 3: # if GLSL 450 not supported, use GLES 300
-        USING_ES = True
+    if major >= 4:  # Assuming GLSL 450
+        shader_folder += "450_CORE"
+    elif major == 3:  # if GLSL 450 not supported, use GLES 300
         shader_folder += "300_ES"
     else:
         raise NotImplementedError("GLSL Version ({major, minor}) is unsupported (too low)!")
 
     # SHADER COMPILATION
-    compile_shader = lambda filename, shader_type: compileShader(open(filename, "rb"), shader_type)
+    def compile_shader(filename, shader_type):
+        return compileShader(open(filename, "rb"), shader_type)
     # Team Fortress 2 & Vindictus FACES
     # brush_shader
     vert_shader = compile_shader(f"{shader_folder}/brush.v", GL_VERTEX_SHADER)
@@ -133,32 +137,30 @@ def main(width, height, bsp):
         bsp_shader = brush_shader
     else:
         bsp_shader = debug_mesh_shader
-    
+
     # SHADER VERTEX FORMAT
     # BSP FACES
-    glEnableVertexAttribArray(0) # vertexPosition
-    glEnableVertexAttribArray(1) # vertexNormal
-    glEnableVertexAttribArray(2) # vertexTexcoord
-    glEnableVertexAttribArray(3) # vertexLightmapCoord
-    glEnableVertexAttribArray(4) # reflectivityColour
+    glEnableVertexAttribArray(0)  # vertexPosition
+    glEnableVertexAttribArray(1)  # vertexNormal
+    glEnableVertexAttribArray(2)  # vertexTexcoord
+    glEnableVertexAttribArray(3)  # vertexLightmapCoord
+    glEnableVertexAttribArray(4)  # reflectivityColour
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 52, GLvoidp(0))
     glVertexAttribPointer(1, 3, GL_FLOAT, GL_TRUE,  52, GLvoidp(12))
     glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 52, GLvoidp(24))
     glVertexAttribPointer(3, 2, GL_FLOAT, GL_FALSE, 52, GLvoidp(32))
     glVertexAttribPointer(4, 3, GL_FLOAT, GL_FALSE, 52, GLvoidp(40))
+
     # rBSP MESHES
-    glEnableVertexAttribArray(5) # vertexPosition
-    glEnableVertexAttribArray(6) # vertexNormal
-    glEnableVertexAttribArray(7) # vertexTexCoord
+    glEnableVertexAttribArray(5)  # vertexPosition
+    glEnableVertexAttribArray(6)  # vertexNormal
+    glEnableVertexAttribArray(7)  # vertexTexCoord
     glVertexAttribPointer(5, 3, GL_FLOAT, GL_FALSE, 32, GLvoidp(0))
     glVertexAttribPointer(6, 3, GL_FLOAT, GL_TRUE,  32, GLvoidp(12))
     glVertexAttribPointer(7, 2, GL_FLOAT, GL_FALSE, 32, GLvoidp(24))
 
     # SHADER UNIFORMS
-##    if USING_ES:
-##        glUseProgram(face_shader)
-##        attrib_position = glGetAttribLocation(brush_shader, "MVP_matrix")
-##        glUseProgram(0)
+    ...
 
     # tracing backwards from a path_track that touches nothing
     # looking at train_watcher / cart for the start would be better
@@ -168,12 +170,12 @@ def main(width, height, bsp):
             if "target" not in entity:
                 start = entity
                 continue
-            next_track = entity["target"] # next path_track
+            next_track = entity["target"]  # next path_track
             tracks[next_track] = entity
     payload_track = [start]
     while len(payload_track) < len(tracks):
-        current_track = payload_track[-1]["targetname"] # name of path_track
-        payload_track.append(tracks[current_track]) # <<< tracing backwards
+        current_track = payload_track[-1]["targetname"]  # name of path_track
+        payload_track.append(tracks[current_track])  # <<< tracing backwards
 
     # INPUT STATE
     keys = []
@@ -220,25 +222,27 @@ def main(width, height, bsp):
             # TICK START
             VIEW_CAMERA.update(mousepos, keys, 1 / tickrate)
             VIEW_CAMERA.speed = max(1, VIEW_CAMERA.speed)
-            if SDLK_BACKQUOTE in keys: # ~: debug print
+            if SDLK_BACKQUOTE in keys:  # ~: debug print
                 print(VIEW_CAMERA)
-                while SDLK_BACKQUOTE in keys: # print once until pressed again
+                while SDLK_BACKQUOTE in keys:  # print once until pressed again
                     keys.remove(SDLK_BACKQUOTE)
-            if SDLK_r in keys: # R: reset camera
+            if SDLK_r in keys:  # R: reset camera
                 VIEW_CAMERA = camera.freecam(*view_init)
-            if SDLK_LSHIFT in keys: # LShift: Increase camera speed
+            if SDLK_LSHIFT in keys:  # LShift: Increase camera speed
                 VIEW_CAMERA.speed += 5
-            if SDLK_LCTRL in keys: # LCtrl: reduce camera speed
+            if SDLK_LCTRL in keys:  # LCtrl: reduce camera speed
                 VIEW_CAMERA.speed -= 5
             # TICK END
             dt -= 1 / tickrate
             end_of_previous_tick = time.time()
 
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+
         glPushMatrix()
         VIEW_CAMERA.set()
         glUseProgram(bsp_shader)
         glDrawElements(GL_TRIANGLES, len(indices), GL_UNSIGNED_INT, GLvoidp(0))
+
         # ORIGIN MARKER
         glUseProgram(0)
         glBegin(GL_LINES)
@@ -253,6 +257,7 @@ def main(width, height, bsp):
         glVertex(0, 0, 128)
         glEnd()
 
+        # ENTITY ORIGINS
         glColor(1, 0, 1)
         glBegin(GL_POINTS)
         for entity in bsp.ENTITIES:
@@ -260,43 +265,58 @@ def main(width, height, bsp):
                 continue
             glVertex(*map(float, entity["origin"].split()))
         glEnd()
+
+        # PATH_TRACK CONNECTIONS
         glBegin(GL_LINE_STRIP)
         for path_track in payload_track:
             glVertex(*map(float, path_track["origin"].split()))
         glEnd()
+
         glPopMatrix()
+
         SDL_GL_SwapWindow(window)
 
 
 if __name__ == "__main__":
-    bsps = []
-    
-    folder = "D:/SteamLibrary/steamapps/common/Team Fortress 2/tf/maps/"
-##    filename = "cp_cloak.bsp"
-##    filename = "cp_coldfront.bsp"
-    folder, filename = "../maps/", "pl_upward.bsp"
-    bsps.append(bsp_tool.bsp(folder + filename, bsp_tool.mods.team_fortress2))
+    bsps = {"E:/Mod/Titanfall/maps/": [],
+            "E:/Mod/Titanfall2/maps/": []}
 
-##    folder = "E:/Mod/Titanfall2/"
-####    mapname = "mp_colony02"
-####    mapname = "mp_crashsite3"
-##    mapname = "mp_drydock"
-####    mapname = "mp_glitch"
-####    mapname = "mp_lf_uma"
-####    mapname = "mp_relic02"
-##    filename = f"{mapname}/maps/{mapname}.bsp"
-##    bsps.append(bsp_tool.bsp(folder + filename, bsp_tool.mods.titanfall2))
-##
-##    folder = "E:/Mod/ApexLegends/maps/"
-####    filename = "mp_rr_canyonlands_staging.bsp"
-####    filename = "mp_rr_canyonlands_mu1_night.bsp"
-##    filename = "mp_rr_canyonlands_mu2.bsp"
-##    bsps.append(bsp_tool.bsp(folder + filename, bsp_tool.mods.apex_legends))
+    for folder in bsps:
+        bsps[folder] = [f"{mapname}.bsp" for mapname in open(f"{folder}../maplist.txt").readlines()]
+
+    bsps.update({"D:/SteamLibrary/steamapps/common/Team Fortress 2/tf/maps/": ["cp_cloak.bsp",
+                                                                               "cp_coldfront.bsp",
+                                                                               "pl_upward.bsp"],
+                 "E:/Mod/ApexLegends/maps/": ["mp_rr_canyonlands_64k_x_64k.bsp",  # King's Canyon
+                                              "mp_rr_desertlands_64k_x_64k.bsp",  # World's Edge
+                                              "olympus.bsp"]})
+
+    # folder = "D:/SteamLibrary/steamapps/common/Team Fortress 2/tf/maps/"
+    # filename = "cp_cloak.bsp"
+    # filename = "cp_coldfront.bsp"
+    # folder, filename = "../maps/", "pl_upward.bsp"
+
+    # folder = "E:/Mod/Titanfall/maps/"
+    # filename = "mp_angel_city.bsp"
+
+    # folder = "E:/Mod/Titanfall2/maps/"
+    # filename = "mp_colony02"
+    # filename = "mp_crashsite3"
+    # filename = "mp_drydock"
+    # filename = "mp_glitch"
+    # filename = "mp_lf_uma"
+    # filename = "mp_relic02"
+
+    # folder = "E:/Mod/ApexLegends/maps/"
+    # filename = "mp_rr_canyonlands_staging.bsp"
+    # filename = "mp_rr_canyonlands_mu1_night.bsp"
+    # filename = "mp_rr_canyonlands_mu2.bsp"
 
     width, height = 1280, 720
-    for bsp in bsps:
-        try:
-            main(width, height, bsp)
-        except Exception as exc:
-            SDL_Quit()
-            raise exc
+    for folder in bsps:
+        for filename in folder:
+            try:
+                main(width, height, bsp_tool.load(os.path.join(folder, filename)))
+            except Exception as exc:
+                SDL_Quit()
+                raise exc
