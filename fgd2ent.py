fbba49ab4de64559a23ade0ed3bd46b21cedde29
Jared Ketterer <36507175+snake-biscuits@users.noreply.github.com>
2022-11-10 19:49:30 +1000

    fgd2ent good enough (interactive, no batch)

--- fgd2ent.py
+++ fgd2ent.py
@@ -1,134 +1,183 @@
-from typing import Any, Dict, Set
+import collections
+import fnmatch
+import os
+# import re
+from typing import Any, Dict, List, Set
+
+import bsp_tool
+import valvefgd
 
 
+# TODO: refactor to work for any game & fgd combination
+# TODO: use variables instead of globals
+r1md = "/media/bikkie/3964-3935/Mod/Titanfall/maps"
+p2fd = "/media/bikkie/3964-3935/Mod/Portal2/bin/base.fgd"
+
+fgd = valvefgd.FgdParse(p2fd)
+fgd_ents = [e.name for e in fgd.entities]
+
+
+def ur_ent(**filters: Dict[str, str]) -> Dict[str, Set[str]]:
+    """Find 'ur' entity (all keys & values used in official maps)"""
+    # TODO: take cached maps: Dict[str, bsp_tool.Bsp] as an argument
+    # NOTE: will get a handful of false positives from renamed ents w/ forgotten key values
+    out = collections.defaultdict(set)
+    for map_name in fnmatch.filter(os.listdir(r1md), "*.bsp"):
+        print("scanning", map_name)
+        bsp = bsp_tool.load_bsp(os.path.join(r1md, map_name))
+        for ent in sum(bsp.search_all_entities(**filters).values(), start=list()):
+            for key, value in ent.items():
+                if key in ("classname", "spawnflags"):
+                    continue
+                out[key].add(value)
+            break  # HACK: gotta test fast
+        break  # HACK: gotta test fast
+    return {k: sorted(out[k]) for k in sorted(out)}
+
+
+def id_ent(classname: str = None, **filters: Dict[str, str]) -> Dict[str, Set[str]]:
+    """build entity dossier"""
+    if classname is not None:
+        filters["classname"] = classname
+    assert "classname" in filters
+    ent_class = filters["classname"]
+    ue = ur_ent(**filters)
+    ue_keys = set(ue.keys())
+    out = {"classname": ent_class, "ur": ue, "type": "point", "spec": None, "old": set(), "new": ue_keys}
+    if ent_class not in fgd_ents:
+        out["origin"] = "Titanfall"
+        if any([v.startswith("*") for v in ue.get("model", list())]):  # brush entity
+            out["new"].remove("model")
+            out["type"] = "group"
+        if "origin" in out["new"]:  # automatically added by Radiant
+            out["new"].remove("origin")
+        return out
+    # TODO: catch editorclass / spawnclass subtype
+    fe = fgd.entity_by_name(ent_class)
+    fe_keys = {p.name for p in fe.properties}
+    out.update({"origin": "Source",
+                "type": {"PointClass": "point", "SolidClass": "group"}[fe.class_type],
+                "new": ue_keys.difference({*fe_keys, "origin"}),
+                "old": fe_keys.difference({*ue_keys, "origin"}),
+                "spec": fe})
+    return out
+
+
+# .ent (XML) assemblers
 def camelCase(snake_case: str) -> str:
+    """choice type name formatter"""
     out = list()
     for word in snake_case.split("_"):
-        out.append(word[0].upper() + word[1:].lower())
+        out.append(word[0].upper() + word[1:])
     out = "".join(out)
     return out[0].lower() + out[1:]
 
 
-def ent_color(fgd, ent_schema: Dict[str, Any]):
-    defs = {d["name"]: d["args"] for d in ent_schema["definitions"]}
-    if "color" in defs:  # found
-        return " ".join([f"{x / 255:.2f}" for x in defs["color"]["args"][0].split()])
-    for base_ent_name in defs.get("base", list())[::-1]:  # last override first
-        base_ent = fgd.entity_by_name(base_ent_name)
-        color = ent_color(base_ent)  # recurse
-        if color is not None:
-            return color
-    return None  # default
+def xml_choices(choice_property: valvefgd.FgdEntityProperty) -> (str, str):
+    type_name = camelCase(choice_property.name)
+    out = [f'<list name="{type_name}">']
+    for choice in choice_property.choices:
+        out.append(f'  <item ="{choice.display_name}" value="{choice.value}"/>')
+    out.append("</list>")
+    return type_name, "\n".join(out)
 
 
-def ent_model(fgd, ent_schema: Dict[str, Any]):
-    """trace BaseClass 'studio("ref.mdl")'"""
-    defs = {d["name"]: d["args"] for d in ent_schema["definitions"]}
-    if "studio" in defs:  # found
-        return defs["studio"]["args"][0]
-    for base_ent_name in defs.get("base", list())[::-1]:  # last override first
-        base_ent = fgd.entity_by_name(base_ent_name)
-        studio = ent_color(base_ent)  # recurse
-        if studio is not None:
-            return studio
-    return None  # default
+log2 = {2 ** i: i for i in range(32)}  # for spawnflags conversion
 
 
-def fgd_point_ent_schema_to_xml(fgd, ent_name: str, r1_dd: Dict[str, Set[str]]) -> str:
+def xml_spawnflags(spawnflags: List[valvefgd.FgdEntitySpawnflag]) -> str:
     out = list()
-    ent_schema = fgd.entity_by_name("ent_name").schema
-    assert ent_schema["class_type"] == "PointClass"
-    # TODO: diff keys (properties) against r1_keys
-    # -- same type
-    # -- TODOs for new keys (or prefill string type)
-    ent_keys = {d["name"] for d in ent_schema["properties"]}
-    r1_keys = set(r1_dd.keys())
-    new_keys = r1_keys.difference(ent_keys)
-    if len(new_keys) > 0:
-        out.append(f"<!-- TODO: {', '.join(new_keys)} -->")
-    ent_keys = ent_keys.difference(r1_keys)
-    color = ent_color(ent_schema)
-    color = "1 0 1" if color is None else color
-    box = "-8 -8 -8 8 8 8"
-    model = ent_model(ent_schema)
-    if model is not None:
-        out.append(f"<!-- TODO: {model=} -->")
-        # TODO: rescale box to model
-    out.append(f'<point name="{ent_name}" color="{color}" box="{box}">')
-    out.append(ent_schema["description"].replace("&", "and"))
+    for flag in spawnflags:
+        # flag.display_name, value, default_value
+        name = "_".join(["FLAG", *map(str.upper, flag.display_name.split())])
+        bit = log2[flag.value]
+        default = int(flag.default_value)
+        out.append(f'<flag name="{name}" bit="{bit}" value="{default}">TODO: get description from VDC</flag>')
+    return out
+
+
+def ent_definitions(ent_spec: valvefgd.FgdEntity) -> Dict[str, Dict[str, Any]]:
+    out = dict()
+    ancestors = list(ent_spec.parents)
+    for ancestor in ancestors:
+        ancestors.extend(ancestor.parents)  # keep digging
+        for defs in getattr(ancestor, "defintions", list()):
+            for d in defs:
+                if d["name"] not in out:  # newest overrides
+                    out[d["name"]] = d["args"][0]
+    return out
+
+
+key_types = {"float": "real", "boolean": "boolean", "integer": "integer", "studio": "model",
+             "sound": "sound", "target_source": "targetname", "target_dest": "targetname"}
+# ^ fgd_key key_type
+
+
+def guess_key_type(key_name: str, key_values: Set[str]) -> str:
+    """ur_key key_type"""
+    is_vec3, is_vec4, is_path = False, False, False
+    if key_name.lower() == "scale":
+        return "real"
+    # TODO: regex numbers
+    if all(map(lambda v: v.count(" ") == 3, key_values)):
+        is_vec3 = True
+    if all(map(lambda v: v.count(" ") == 4, key_values)):
+        is_vec4 = True
+    if any(map(lambda v: "/" in v.replace("\\", "/"), key_values)):
+        is_path = True
+    if "color" in key_name.lower() and is_vec3 and not is_vec4:
+        return "color"
+    if "model" in key_name.lower() and is_path:
+        return "model"
+    if "target" in key_name.lower():  # TEST: false positives
+        return "targetname"
+    return "string"
+
+
+def xml_ent(classname: str = None, **filters: Dict[str, str]) -> (str, Set[str]):
+    out = list()
+    choice_types = set()
+    dossier = id_ent(classname, **filters)
+    defs = ent_definitions(dossier["spec"]) if dossier["spec"] is not None else dict()
+    head = f"<{dossier['type']} name=\"{dossier['classname']}\""
+    color = " ".join(map(str, defs.get("color", [1, 0, 1])))
+    bonus = list()
+    if "studio" in defs:  # editor model
+        studio = defs["studio"]
+        out.append(f'<!-- TODO: convert {studio} to .obj -->')
+        bonus.append(f'model="{studio}"')
+    elif dossier["type"] == "point":
+        bonus.append('box="-8 -8 -8 8 8 8"')
+    out.append(" ".join([head, color, *bonus]) + ">")
+    if dossier["spec"] is not None:  # source based
+        out.append(dossier["spec"].description.replace("&", "and"))
+        fgd_keys = [p for p in dossier["spec"].properties if p.name not in dossier["old"]]
+        spawnflags = dossier["spec"].spawnflags
+    else:  # dummy spec
+        fgd_keys = list()
+        spawnflags = list()
     out.append("----- KEYS -----")
-    for key_name in ent_keys:
-        key = ent_schema["properties"][key_name]
-        key_d_name = key["display_name"]
-        if key_name == "id":
-            continue
-        key_default = key["default_falue"]
-        key_default = f' value="{key_default}"' if key_default != "" else ""
-        key_desc = key["description"].replace("&", "and")
-        # TODO: key_type & confirm r1_dd type matches
-        out.append(f'<{key_type} key="{key_name}" name="{key_d_name}"{key_default}>{key_desc}</{type}>')
-        # TODO: value if default exists
-        if "choices" in key:
-            # choices -> list
-            choice = [f"<!-- {ent_name} -->",
-                      f'<list name="{camelCase(key_d_name)}">',
-                      *[f'''  <item name="{c['name']}" value="{c['value']}"/>''' for c in key["choices"]],
-                      "</list>"]
-            out.insert(0, "\n".join(choice) + "\n\n")
+    for key in fgd_keys:
+        if key.value_type == "choices":
+            key_type, choice_list = xml_choices(key)
+            choice_types.add(choice_list)
+        else:
+            key_type = key_types.get(key.value_type, "string")
+        out.append(" ".join([f'<{key_type} key="{key.name}" name="{key.display_name}"',
+                             f'value="{key.default_value}">{key.description}</{key_type}>']))
+    for key_name in dossier["new"]:  # ur_keys
+        key_type = guess_key_type(key_name, dossier["ur"][key_name])
+        out.append(f'<{key_type} key="{key_name}" name="{key_name}">New in Titanfall; TODO: identify use</{key_type}>')
     # spawnflags
-    if "spawnflags" in ent_schema:
+    if len(spawnflags) > 0:
         out.append("----- SPAWNFLAGS -----")
-        for flag in ent_schema["spawnflags"]:
-            flag_name = "FLAG_{flag['value']}"
-            # NOTE: any multi-bit flag will break here, which is fine by me
-            log2 = {2**i: i for i in range(32)}
-            flag_bit = log2[flag["value"]]
-            flag_default = flag["default_value"]
-            flag_default = 0 if flag_default == "" else int(flag_default)
-            flag_desc = flag["display_name"].replace("&", "and")
-            out.append(f'<flag name="{flag_name}" bit="{flag_bit}" value="{flag_default}">{flag_desc}</flag>')
+        out.extend(xml_spawnflags(spawnflags))
     out.append("----- NOTES -----")
-    out.append("Inherited from Source")
-    # TODO: verify XML (use Notepad++ XMLTools plugin)
-    return "\n".join(out)
-
-
-if __name__ == "__main__":
-    import collections
-    import fnmatch
-    import functools
-    import os
-
-    import bsp_tool
-    import valvefgd
-
-    # TODO: switch paths depending on uname
-    # cache all maps; uses a lot of RAM!
-    r1md = "/media/bikkie/3964-3935/Mod/Titanfall/maps"
-    r1m = {m[:-4]: bsp_tool.load_bsp(os.path.join(r1md, m)) for m in fnmatch.filter(os.listdir(r1md), "*.bsp")}
-    r1omd = "/media/bikkie/3964-3935/Mod/TitanfallOnline/maps"
-    r1om = {m[:-4]: bsp_tool.load_bsp(os.path.join(r1omd, m)) for m in fnmatch.filter(os.listdir(r1omd), "*.bsp")}
-    maps = {**{f"r1/{m}": b for m, b in r1m.items()}, **{f"r1o/{m}": b for m, b in r1om.items()}}
-    del r1m, r1om
-
-    fgd = valvefgd.FgdParse("/home/bikkie/Documents/Code/GitHub/QtPyHammer/Team Fortress 2/bin/base.fgd")
-
-    ent_file = open("out.ent", "w")
-
-    for ent in fgd.entities:
-        ent_name = ent["name"]
-        # similar to fgd_gen_files/findings.txt
-        r1_ent_instances = [e for b in r1m.values() for e in sum(b.search_all_entities(classname=ent_name).values())]
-        r1_ent_keys = functools.reduce(lambda a, b: a.union(b), [set(e.keys()) for e in r1_ent_instances])
-        r1_ent_dd = collections.defaultdict(set)
-        for key in r1_ent_keys:
-            if key in ("classname", "origin", "angles"):
-                continue  # we know what those do
-            for instance in r1_ent_instances:
-                r1_ent_dd[key].add(instance.get(key, ""))  # sample every key
-        if ent.schema["class_type"] == "PointClass":
-            xml_snippet = fgd_point_ent_schema_to_xml(fgd, ent_name, dict(r1_ent_dd))
-            ent_file.write(xml_snippet)
-        del r1_ent_instances
-
-    ent_file.close()
+    out.append(f"Introduced by {dossier['origin']}")
+    if dossier["origin"] == "Source":
+        # TODO: find a ratio at which you can safely say: "total refactor"
+        out.extend([f"Added: {', '.join(dossier['new'])}",
+                    f"Removed: {', '.join(dossier['old'])}",
+                    "TODO: speculate on changes"])
+    out.append(f'</{dossier["type"]}>')
+    return "\n".join(out), choice_types
