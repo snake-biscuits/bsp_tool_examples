f0543fae88039c1e48ec33076b6cd72d26c5080c
snake-biscuits <36507175+snake-biscuits@users.noreply.github.com>
2023-06-03 08:46:35 +0100

    cleaning up r1/r1o asset checking scripts

--- respawn/r1_vertex_vmt_matcher.py
+++ respawn/r1_vertex_vmt_matcher.py
@@ -1,14 +1,19 @@
-from collections import defaultdict
+# NOTE: path capitalisation may make things tricky on linux
 import fnmatch
 import os
 import re
-import sys
-sys.path.append("../bsp_tool")
-import bsp_tool  # noqa E402
+
+import bsp_tool
 
 
-missing = set()
-results = set()
+r1_materials = "E:/Mod/Titanfall/materials"
+r1o_materials = "E:/Mod/TitanfallOnline/TitanFallOnline/assets_dump/materials"
+
+shader_pattern = re.compile(r'([a-zA-Z_]+)|"([a-zA-Z_]+)"')
+
+# search
+results, missing = set(), set()
+bad_parse = set()
 for md in ("E:/Mod/Titanfall/maps", "E:/Mod/TitanfallOnline/maps"):
     print(f"> {md}")
     for map_name in fnmatch.filter(os.listdir(md), "*.bsp"):
@@ -16,23 +21,36 @@ for md in ("E:/Mod/Titanfall/maps", "E:/Mod/TitanfallOnline/maps"):
         bsp = bsp_tool.load_bsp(os.path.join(md, map_name))
         for td in bsp.TEXTURE_DATA:
             vertex_type = (td.flags & bsp_tool.branches.respawn.titanfall.MeshFlags.MASK_VERTEX).name
-            mat_name = bsp.TEXTURE_DATA_STRING_DATA[td.name_index]
-            mat_dir = "E:/Mod/TitanfallOnline/TitanFallOnline/assets_dump/materials"
-            mat_path = f"{mat_dir}/{bsp.TEXTURE_DATA_STRING_DATA[td.name_index]}.vmt"
-            if not os.path.exists(mat_path):
-                missing.add(mat_name.lower())
-                continue
-            with open(mat_path, "r") as vmt_file:
-                for line in vmt_file:
-                    m = re.match(r'"(.*)"', line)  # shader
-                    if m is not None:
-                        shader = m.groups()[0]
-                        results.add((vertex_type, shader))
-                        continue
+            vmt_name = bsp.TEXTURE_DATA_STRING_DATA[td.name_index].lower()
+            vmt_path = f"{bsp.TEXTURE_DATA_STRING_DATA[td.name_index]}.vmt"
+            if not os.path.exists(os.path.join(r1_materials, vmt_path)):
+                if not os.path.exists(os.path.join(r1o_materials, vmt_path)):
+                    missing.add(vmt_name)
+                    continue
+                else:
+                    vmt_path = os.path.join(r1o_materials, vmt_path)
+            else:
+                vmt_path = os.path.join(r1_materials, vmt_path)
+            with open(vmt_path, "r") as vmt_file:
+                matches = {shader_pattern.match(line) for line in vmt_file}
+                if matches == {None}:
+                    bad_parse.add(vmt_name)
+                else:
+                    assert len(matches) == 2, f"{vmt_name}: {matches}"
+                    shader = [g for g in list(matches.difference({None}))[0].groups() if g is not None][0]
+                    assert shader is not None, f"{vmt_name}: {matches}"
+                    results.add((vertex_type, shader))
 
-print("*** SKIPPED ***")
-{print(m) for m in sorted(missing)}
+
+# report
+if len(missing) > 0:
+    print("*** NOT FOUND ***")
+    {print(m) for m in sorted(missing)}
+if len(bad_parse) > 0:
+    print("*** BAD PARSE ***")
+    {print(m) for m in sorted(bad_parse)}
 print("-" * 35)
 {print(f"{vertex_type:<16} {shader}") for vertex_type, shader in sorted(results)}
 print("-" * 35)
 print(f"failed to locate {len(missing)} .vmts")
+print(f"failed to parse {len(bad_parse)} .vmts")
--- respawn/r1o_missing_assets.py
+++ respawn/r1o_missing_assets.py
@@ -2,10 +2,8 @@ from collections import defaultdict
 import fnmatch
 import os
 import re
-import sys
-sys.path.append("../../bsp_tool")
-import bsp_tool  # noqa E402
-from bsp_tool.extensions.archives import respawn  # noqa E402
+
+from bsp_tool.extensions import archives  # v0.5.0 / github@latest
 
 
 r1_vpk_dir = "D:/SteamLibrary/steamapps/common/Titanfall/vpk"
@@ -14,7 +12,7 @@ r1o_dir = "E:/Mod/TitanfallOnline/TitanFallOnline/assets_dump"
 missing = defaultdict(set)
 # ^ {"vpk_name": {"folder/asset.ext"}}
 for vpk_name in fnmatch.filter(os.listdir(r1_vpk_dir), "englishclient_*.pak000_dir.vpk"):
-    vpk = respawn.Vpk()
+    vpk = archives.respawn.Vpk(vpk_name)
     vpk_name_short = re.match(r"englishclient_(.*).bsp.pak000_dir.vpk").groups()[0]
     for asset_path in vpk.namelist():
         if asset_path.startswith("depot"):
