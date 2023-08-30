76dd1e12253cbb97e131a94f48709d1c13e3c236
snake-biscuits <36507175+snake-biscuits@users.noreply.github.com>
2023-08-30 08:29:25 +0100

    (respawn.gltf) vertex buffer refactor

--- respawn/gltf.cpp
+++ respawn/gltf.cpp
@@ -1,5 +1,6 @@
 #include <cstdio>
 #include <cstring>
+#include <vector>
 
 #include "gltf.hpp"
 
@@ -13,6 +14,7 @@ typedef struct {
     float    lightmap_step[2];  // float          VEC2  TEXCOORD_2
     // theory: lightmap_step is added to lightmap uv (increments w/ style)
     // TODO: int32_t  tangent[2];  => float  tangent[4]  (float VEC4 TANGENT)
+    // theory: tangent is .rg values of normal map, sampled at vertex
 } VertexGLTF;
 
 
@@ -73,44 +75,42 @@ int main(int argc, char* argv[]) {
     // vertex buffer
     std::vector<VertexGLTF>  vertex_buffer;
     VertexGLTF  gltf_vertex;
-    // TODO: VEC4 tangent & maybe lightmap step
+    // TODO: convert position to .gltf coordinate system (Y+ up / rotate -90 X)
+    // NOTE: we could do a unique VertexStruct & accessors for each Vertex type
+    // -- this would reduce filesize, at the cost of splitting up meshes
+    #define COPY_VERTEX_BASE() \
+        Vertex  z_up = VERTICES[bsp_vertex.position_index];\
+        gltf_vertex.position.x =  z_up.x; \
+        gltf_vertex.position.y =  z_up.z; \
+        gltf_vertex.position.z = -z_up.y; \
+        gltf_vertex.normal   = VERTEX_NORMALS[bsp_vertex.normal_index]; \
+        memcpy(gltf_vertex.uv,     bsp_vertex.uv,     sizeof(float)   * 2); \
+        memcpy(gltf_vertex.colour, bsp_vertex.colour, sizeof(uint8_t) * 4)
     for (const auto &bsp_vertex : VERTEX_UNLIT) {
-       gltf_vertex.position = VERTICES[bsp_vertex.position_index];
-       gltf_vertex.normal   = VERTEX_NORMALS[bsp_vertex.normal_index];
-       memcpy(gltf_vertex.uv,     bsp_vertex.uv,     sizeof(float)   * 2);
-       memcpy(gltf_vertex.colour, bsp_vertex.colour, sizeof(uint8_t) * 4);
-       memset(gltf_vertex.lightmap_uv,   0, sizeof(float) * 2);
-       memset(gltf_vertex.lightmap_step, 0, sizeof(float) * 2);
-       vertex_buffer.emplace_back(gltf_vertex);
+        COPY_VERTEX_BASE();
+        memset(gltf_vertex.lightmap_uv,   0, sizeof(float) * 2);
+        memset(gltf_vertex.lightmap_step, 0, sizeof(float) * 2);
+        vertex_buffer.emplace_back(gltf_vertex);
     }
     for (const auto &bsp_vertex : VERTEX_LIT_FLAT) {
-       gltf_vertex.position = VERTICES[bsp_vertex.position_index];
-       gltf_vertex.normal   = VERTEX_NORMALS[bsp_vertex.normal_index];
-       memcpy(gltf_vertex.uv,            bsp_vertex.uv,            sizeof(float)   * 2);
-       memcpy(gltf_vertex.colour,        bsp_vertex.colour,        sizeof(uint8_t) * 4);
-       memcpy(gltf_vertex.lightmap_uv,   bsp_vertex.lightmap.uv,   sizeof(float)   * 2);
-       memcpy(gltf_vertex.lightmap_step, bsp_vertex.lightmap.step, sizeof(float)   * 2);
-       vertex_buffer.emplace_back(gltf_vertex);
+        COPY_VERTEX_BASE();
+        memcpy(gltf_vertex.lightmap_uv,   bsp_vertex.lightmap.uv,   sizeof(float) * 2);
+        memcpy(gltf_vertex.lightmap_step, bsp_vertex.lightmap.step, sizeof(float) * 2);
+        vertex_buffer.emplace_back(gltf_vertex);
     }
     for (const auto &bsp_vertex : VERTEX_LIT_BUMP) {
-       gltf_vertex.position = VERTICES[bsp_vertex.position_index];
-       gltf_vertex.normal   = VERTEX_NORMALS[bsp_vertex.normal_index];
-       memcpy(gltf_vertex.uv,            bsp_vertex.uv,            sizeof(float)   * 2);
-       memcpy(gltf_vertex.colour,        bsp_vertex.colour,        sizeof(uint8_t) * 4);
-       memcpy(gltf_vertex.lightmap_uv,   bsp_vertex.lightmap.uv,   sizeof(float)   * 2);
-       memcpy(gltf_vertex.lightmap_step, bsp_vertex.lightmap.step, sizeof(float)   * 2);
-       // TODO: convert tangent to VEC4
-       vertex_buffer.emplace_back(gltf_vertex);
+        COPY_VERTEX_BASE();
+        memcpy(gltf_vertex.lightmap_uv,   bsp_vertex.lightmap.uv,   sizeof(float) * 2);
+        memcpy(gltf_vertex.lightmap_step, bsp_vertex.lightmap.step, sizeof(float) * 2);
+        // TODO: tangent
+        vertex_buffer.emplace_back(gltf_vertex);
     }
     for (const auto &bsp_vertex : VERTEX_UNLIT_TS) {
-       gltf_vertex.position = VERTICES[bsp_vertex.position_index];
-       gltf_vertex.normal   = VERTEX_NORMALS[bsp_vertex.normal_index];
-       memcpy(gltf_vertex.uv,     bsp_vertex.uv,     sizeof(float)   * 2);
-       memcpy(gltf_vertex.colour, bsp_vertex.colour, sizeof(uint8_t) * 4);
-       memset(gltf_vertex.lightmap_uv,   0, sizeof(float) * 2);
-       memset(gltf_vertex.lightmap_step, 0, sizeof(float) * 2);
-       // TODO: convert tangent to VEC4
-       vertex_buffer.emplace_back(gltf_vertex);
+        COPY_VERTEX_BASE();
+        memset(gltf_vertex.lightmap_uv,   0, sizeof(float) * 2);
+        memset(gltf_vertex.lightmap_step, 0, sizeof(float) * 2);
+        // TODO: tangent
+        vertex_buffer.emplace_back(gltf_vertex);
     }
     unsigned int VERTEX_UNLIT_OFFSET    = 0;
     unsigned int VERTEX_LIT_FLAT_OFFSET = VERTEX_UNLIT.size();
@@ -120,6 +120,9 @@ int main(int argc, char* argv[]) {
     // index buffer
     std::vector<uint32_t>  index_buffer;
     auto  worldspawn = MODELS[0];
+    // TODO: mesh.primitive group tracking
+    // -- (material, lightmap_index, first_index, num_indices)
+    // splitting worldspawn (opaque, transparent, decals, sky etc.) could be nice
     // TODO: metadata
     // -- worldspawn.min & max -> bufferView min & max
     for (int j = 0; j <= static_cast<int>(worldspawn.num_meshes); j++) {
@@ -130,6 +133,7 @@ int main(int argc, char* argv[]) {
             case MeshFlags::VERTEX_LIT_FLAT:  gltf_offset = VERTEX_LIT_FLAT_OFFSET;  break;
             case MeshFlags::VERTEX_LIT_BUMP:  gltf_offset = VERTEX_LIT_BUMP_OFFSET;  break;
             case MeshFlags::VERTEX_UNLIT_TS:  gltf_offset = VERTEX_UNLIT_TS_OFFSET;  break;
+            default:  gltf_offset = 0;  // TODO: std::unreachable (C++23)
         }
         auto material_sort = MATERIAL_SORTS[mesh.material_sort];
         const int start = static_cast<int>(mesh.first_mesh_index);
@@ -159,36 +163,37 @@ int main(int argc, char* argv[]) {
     // write .gltf
     FILE* gltf_file = fopen("bsp.gltf", "w");
     fprintf(gltf_file, "{\"scene\": 0, \"scenes\": [{\"nodes\": [0]}], \"nodes\": [{\"mesh\": 0}],\n");
-    fprintf(gltf_file, " \"meshes\": [{\"primitives\": [{\"attributes\": {\n");
-    fprintf(gltf_file, "    \"POSITION\": 1,\n");  // accessor indices
-    fprintf(gltf_file, "    \"NORMAL\": 2,\n");
-    fprintf(gltf_file, "    \"TEXCOORD_0\": 3,\n");
-    fprintf(gltf_file, "    \"COLOR_0\": 4,\n");
-    fprintf(gltf_file, "    \"TEXCOORD_1\": 5,\n");
-    fprintf(gltf_file, "    \"TEXCOORD_2\": 6\n");
-    fprintf(gltf_file, "  }, \"indices\": 0}]}],\n");
+    fprintf(gltf_file, " \"meshes\": [{\"primitives\": [\n");
+    fprintf(gltf_file, "{\"attributes\": {\"POSITION\": 0, \"NORMAL\": 1, \"TEXCOORD_0\": 2, " \
+                       "\"COLOR_0\": 3, \"TEXCOORD_1\": 4, \"TEXCOORD_2\": 5}, \"indices\": 6}]}],\n");
+    // NOTE: gltf.mesh.primitive.attribute & indices index into gltf.accessors
+    // TODO: 1 gltf.mesh per bsp.model
+    // TODO: translate gltf.mesh to bsp.model origin (requires entity parsing)
+    // -- could update the .json w/ bsp_tool
+    // TODO: 1 gltf.mesh.primitive per bsp.model.mesh
+    // TODO: 1 gltf.material per bsp.material_sort (unique material_name + lightmap_index)
     fprintf(gltf_file, " \"buffers\": [\n");
-    fprintf(gltf_file, "  {\"uri\": \"bsp.index.bin\", \"byteLength\": %lu},\n", index_buffer_length);
-    fprintf(gltf_file, "  {\"uri\": \"bsp.vertex.bin\", \"byteLength\": %lu}],\n", vertex_buffer_length);
+    fprintf(gltf_file, "  {\"uri\": \"bsp.vertex.bin\", \"byteLength\": %lu},\n", vertex_buffer_length);
+    fprintf(gltf_file, "  {\"uri\": \"bsp.index.bin\", \"byteLength\": %lu}],\n", index_buffer_length);
     fprintf(gltf_file, " \"bufferViews\": [\n");
-    fprintf(gltf_file, "  {\"buffer\": 0, \"byteLength\": %lu, \"target\": %d},\n",
-                       index_buffer_length, ELEMENT_ARRAY_BUFFER);
-    fprintf(gltf_file, "  {\"buffer\": 1, \"byteLength\": %lu, \"target\": %d, \"byteStride\": %lu}],\n",
+    fprintf(gltf_file, "  {\"buffer\": 0, \"byteLength\": %lu, \"target\": %d, \"byteStride\": %lu},\n",
                        vertex_buffer_length, ARRAY_BUFFER, sizeof(VertexGLTF));
+    fprintf(gltf_file, "  {\"buffer\": 1, \"byteLength\": %lu, \"target\": %d}],\n",
+                       index_buffer_length, ELEMENT_ARRAY_BUFFER);
     // TODO: accessor mins & maxs
     fprintf(gltf_file, " \"accessors\": [\n");
-    fprintf(gltf_file, "  {\"bufferView\": 0, \"count\": %lu, \"type\": \"SCALAR\", \"componentType\": %d},\n",
-                       index_buffer.size(), UNSIGNED_INT);
-    #define VERTEX_ATTR(T1, T2, m, e)  fprintf(gltf_file, \
-        "  {\"bufferView\": 1, \"count\": %lu, \"type\": \"%s\", \"componentType\": %d, \"byteOffset\": %lu%s\n", \
-        vertex_buffer.size(), #T1, T2, offsetof(VertexGLTF, m), e)
-    VERTEX_ATTR(VEC3, FLOAT,         position,      "}," );
-    VERTEX_ATTR(VEC3, FLOAT,         normal,        "}," );
-    VERTEX_ATTR(VEC2, FLOAT,         uv,            "}," );
-    VERTEX_ATTR(VEC4, UNSIGNED_BYTE, colour,        "}," );
-    VERTEX_ATTR(VEC2, FLOAT,         lightmap_uv,   "}," );
-    VERTEX_ATTR(VEC2, FLOAT,         lightmap_step, "}],");
+    #define VERTEX_ATTR(T1, T2, m)  fprintf(gltf_file, \
+        "  {\"bufferView\": 0, \"count\": %lu, \"type\": \"%s\", \"componentType\": %d, \"byteOffset\": %lu},\n", \
+        vertex_buffer.size(), #T1, T2, offsetof(VertexGLTF, m))
+    VERTEX_ATTR(VEC3, FLOAT,         position);
+    VERTEX_ATTR(VEC3, FLOAT,         normal);
+    VERTEX_ATTR(VEC2, FLOAT,         uv);
+    VERTEX_ATTR(VEC4, UNSIGNED_BYTE, colour);
+    VERTEX_ATTR(VEC2, FLOAT,         lightmap_uv);
+    VERTEX_ATTR(VEC2, FLOAT,         lightmap_step);
     #undef VERTEX_ATTR
+    fprintf(gltf_file, "  {\"bufferView\": 1, \"count\": %lu, \"type\": \"SCALAR\", \"componentType\": %d}],\n",
+                       index_buffer.size(), UNSIGNED_INT);
     fprintf(gltf_file, " \"asset\": {\"version\": \"2.0\"}}\n");
     fclose(gltf_file);
     return 0;
